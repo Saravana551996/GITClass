b73ee9474be53203cc848a3bdaf1f2eadd8f7b9e
RabiJos <rabijos96@gmail.com>
2021-11-14 00:23:35 +0530

    Rabi Code

---
+++ src/test/java/com/hexa/Rabi.java
@@ -0,0 +1,16 @@
+package com.hexa;
+
+public class Rabi {
+private void method1() {
+	// TODO Auto-generated method stub
+
+}
+private void method2() {
+	// TODO Auto-generated method stub
+
+}
+private void method3() {
+	// TODO Auto-generated method stub
+
+}
+}
