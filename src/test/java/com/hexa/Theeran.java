79b29c1efd07b079a8af15cbbea2babc85dee312
Theeran <theeran23@gmail.com>
2021-11-14 00:01:20 +0530

    Theeran Code

--- src/test/java/com/hexa/Theeran.java
+++ src/test/java/com/hexa/Theeran.java
@@ -1,5 +1,16 @@
 package com.hexa;
 
 public class Theeran {
+private void method1() {
+	// TODO Auto-generated method stub
 
+}
+private void method2() {
+	// TODO Auto-generated method stub
+
+}
+private void method3() {
+	// TODO Auto-generated method stub
+
+}
 }
