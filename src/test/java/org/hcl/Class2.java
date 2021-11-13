5a3cf24346b8b235faf30c6987735778841895ae
Mohanapriya161 <vmohanapriya50gmail.com>
2021-11-13 19:49:44 +0100

    Latest code

---
+++ src/test/java/org/hcl/Class2.java
@@ -0,0 +1,18 @@
+package org.hcl;
+
+public class Class2 {
+	
+	private void method20() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void method21() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method22() {
+		// TODO Auto-generated method stub
+
+	}
+}
