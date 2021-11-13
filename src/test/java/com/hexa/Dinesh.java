1535d12deff5f78e94af306c8a10366b87df2640
Dinesh1956 <dinesharsole02@gmail.com>
2021-11-13 16:56:16 +0530

    push by Dinesh

---
+++ src/test/java/com/hexa/Dinesh.java
@@ -0,0 +1,17 @@
+package com.hexa;
+
+public class Dinesh {
+	public void method1() {
+	System.out.println("Test 1");
+
+	}
+	public  void method2() {
+	System.out.println("Test 2");
+
+	}
+	public void method3() {
+	System.out.println("Test 3");
+	}
+	
+
+}
