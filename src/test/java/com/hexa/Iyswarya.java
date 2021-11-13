764b8625c37f44cd7ca6a044ba98c215af8c2887
Iyswarya311094 <aishwaryaram31@gmail.com>
2021-11-13 18:38:30 +0530

    Iyswarya code

---
+++ src/test/java/com/hexa/Iyswarya.java
@@ -0,0 +1,18 @@
+package com.hexa;
+
+public class Iyswarya {
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+}
