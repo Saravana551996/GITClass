577b03899dd30b3d1047f152eaf597818f8a9748
DurgaDurairaj <durgadurairaj23@gmail.com>
2021-11-13 18:30:35 +0530

    Durgaupdated code

---
+++ src/test/java/com/hexa/Durgadd.java
@@ -0,0 +1,13 @@
+package com.hexa;
+
+public class Durgadd {
+	private void methodA() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodB() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
