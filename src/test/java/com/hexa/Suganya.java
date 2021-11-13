363d56f05c86ef2bcf2533db4c4189c49e5059d0
suganyanantha <nk.suganya90@gmail.com>
2021-11-13 20:34:03 +0530

    Suganya Code

---
+++ src/test/java/com/hexa/Suganya.java
@@ -0,0 +1,19 @@
+package com.hexa;
+
+public class Suganya {
+	
+	public void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	public void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	public void method3() {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
