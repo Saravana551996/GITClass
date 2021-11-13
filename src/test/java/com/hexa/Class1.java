c9044a8ef39c73a1cf11c08f8cfeaeed78c3b0e6
jebajames10 <jeba.james10@gmail.com>
2021-11-13 17:48:41 +0530

    jeba code

--- src/test/java/com/hexa/Class1.java
+++ src/test/java/com/hexa/Class1.java
@@ -58,4 +58,16 @@ public class Class1 {
 		// TODO Auto-generated method stub
 
 	}
+	private void methodOne() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodTwo() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodThree() {
+		// TODO Auto-generated method stub
+
+	}
 }
