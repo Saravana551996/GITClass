a62f1921cc8c7af9048c3f499201a900aaf8abec
sarathkumarbalaji <sarathkumarramuk2@gmail.com>
2021-11-14 18:50:46 +0530

    sarath code

---
+++ src/test/java/com/hexa/Sarath.java
@@ -0,0 +1,16 @@
+package com.hexa;
+
+public class Sarath {
+	private void method() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+}
