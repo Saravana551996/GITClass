302fa631c6f1683a610b1e3fb0bc8d6c1c172434
CharlesXavier1618 <vinotvinot704@gmail.com>
2021-11-15 15:36:17 +0530

    push charles new code

---
+++ src/test/java/com/hexa/Charles.java
@@ -0,0 +1,18 @@
+package com.hexa;
+
+public class Charles {
+
+	private void methodA() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodB() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodC() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
