df10cb795ac60f0dd34e926a909cac75ee416c62
CharlesXavier1618 <vinotvinot704@gmail.com>
2021-11-15 16:27:15 +0530

    Charles push newly updated code

---
+++ src/test/java/com/hexa/CharlesXav.java
@@ -0,0 +1,18 @@
+package com.hexa;
+
+public class CharlesXav {
+
+	public void setupBrowser() {
+		// TODO Auto-generated method stub
+
+	}
+	public void launchBrowser() {
+		// TODO Auto-generated method stub
+
+	}
+	public void type() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
