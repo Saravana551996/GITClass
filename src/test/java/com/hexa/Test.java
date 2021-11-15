c8e9a2afa863ed62988bd2b756a2f0c0b505c10b
Logesh1392 <lokesh.waran1392@gmail.com>
2021-11-15 22:01:21 +0530

    LogeshCode

---
+++ src/test/java/com/hexa/Test.java
@@ -0,0 +1,21 @@
+package com.hexa;
+
+public class Test {
+
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+		
+	
+	}
+
+
