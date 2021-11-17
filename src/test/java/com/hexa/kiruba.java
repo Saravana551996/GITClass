ae87094716ad639c64f8bc6021ed9b3cf868ac5a
kiruba-ramakrishnan <kirubaece99@gmail.com>
2021-11-17 15:25:26 +0530

    update code

---
+++ src/test/java/com/hexa/kiruba.java
@@ -0,0 +1,16 @@
+package com.hexa;
+
+public class kiruba {
+	public void test1() {
+		System.out.println("leve1");
+	}
+	public void test2() {
+		System.out.println("level2");
+
+	}
+	public void test3() {
+		System.out.println("level3");
+
+	}
+
+}
